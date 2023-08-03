c2c95ad5df76e2d7ac67049f601c3581cc2fd3ab
kumarvkt <kumartejaayodhya@gmail.com>
2023-08-03 16:17:44 +0530

    line

---
+++ command line.java
@@ -0,0 +1,15 @@
+
+ class CommandLine
+{
+public static void main(string args[]);
+if (args.length>0);
+{
+System.out.println ("The command line arguments"+"arguments are:");
+     for (int i=0;i<args.length;i++)
+System.out.println(args[i]);
+Systewm.out.prinln(args.length);
+}
+else
+System.out.prinln("No command line arguments found");
+}
+}
